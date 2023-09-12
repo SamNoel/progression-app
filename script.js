311fcbaec1e95e285d471590cfd421f6042aaf60
SamNoel <32908864+SamNoel@users.noreply.github.com>
2023-09-11 22:05:16 -0600

    Drilldown, more functionality and cleanup

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+class Step {
+    constructor(stepName, checked) {
+        this.stepName = stepName;
+        this.checked = checked;
+    }
+}
+
+class Progression {
+    constructor(progressionName, category, percentComplete) {
+        this.progressionName = progressionName;
+        this.category = category;
+        this.percentComplete = percentComplete;
+        //this.step = new Array[Step];
+    }
+}
+
+
 //MOVE THIS TO A LOOP FOR EACH PROGRESSION
 let circularProgress = document.querySelector(".circular-progress"),
     progressValue = document.querySelector(".progress-value");
@@ -19,35 +36,31 @@ let progress = setInterval(() => {
     }
 }, speed);
 
-/*
-    When dynamically populating list, add this:
-    <li class="element-progression-incomplete">
-        <div class="element-left"> <!--left side-->
-            <i class="fa-solid fa-ellipsis-vertical"></i> <!--to house move icon-->
-            <p class="title-progression">Progression Name</p> <!--will be dynamic-->    
-        </div>
-        <div class="circular-progress"> <!--right side-->
-            <span class="progress-value"></span>
-        </div>
-    </li>
-*/
 
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("id-active-progression-input-box");
+const listContainer = document.getElementById("id-active-progressions-list");
+
+var textboxInput = document.getElementById("id-active-progression-input-box");
+    textboxInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {  
+        addProgression(e);
+    }
+  });
 
 function addProgression(){
     if(inputBox.value === ''){
         alert("You must add text to create a progression.");
     }
     else{
+        let newProgression = new Progression(inputBox.value, '', 0);
         let li = document.createElement("li");
-        li.innerHTML = `<li class="element-progression-incomplete">
+        li.innerHTML = `<li class="list-item active-progression-item">
         <div class="element-left"> <!--left side-->
             <i class="fa-solid fa-ellipsis-vertical"></i> <!--to house move icon-->
             <p class="title-progression">${inputBox.value}</p> <!--will be dynamic-->    
         </div>
-        <div class="circular-progress"> <!--right side-->
-            <span class="progress-value"></span>
+        <div class="circular-progress" style="background: conic-gradient(#41A128 ${newProgression.percentComplete * 3.6}deg, #FFF 0deg);"> <!--right side-->
+            <span class="progress-value">${newProgression.percentComplete}%</span>
         </div>
         </li>`;
         listContainer.appendChild(li);
@@ -55,4 +68,41 @@ function addProgression(){
 
     //clear the text box
     inputBox.value = '';
+}
+
+
+var textboxInputStep = document.getElementById("input-box-step");
+const listStepContainer = document.getElementById("id-steps-list");
+textboxInputStep.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {  
+        addStep(e);
+    }
+  });
+
+function addStep(){
+    if(textboxInputStep.value === ''){
+        alert("You must add text to create a step.");
+    }
+    else{
+        let newStep = new Step(textboxInputStep.value, false)
+        let li = document.createElement("li");
+        li.innerHTML = `<li class="list-item step-item">
+
+        <div class="element-left"> <!--left side-->
+            <i class="fa-solid fa-ellipsis-vertical"></i> <!--to house move icon-->
+            <p class="title-progression">${newStep.stepName}</p> <!--will be dynamic-->    
+        </div>
+        <span class="completion-circle" onclick="checkOffStep(this)"></span>
+
+    </li>`;
+        listStepContainer.appendChild(li);
+    }
+
+    //clear the text box
+    textboxInputStep.value = '';
+}
+
+
+function checkOffStep(e){
+    $(e).parent().toggleClass("checked");
 }
